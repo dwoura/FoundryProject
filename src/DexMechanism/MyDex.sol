cc8fcdd08734a791a1650400ec8f8e9c890c89c4
dwoura <568722524@qq.com>
2024-11-13 21:59:13 +0800

    feat: update

---
+++ src/DexMechanism/MyDex.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {IDex} from "src/DexMechanism/IDex.sol";
+contract MyDex{
+
+    constructor(){
+
+    }
+
+    function sellETH(address buyToken,uint256 minBuyAmount) external payable {
+
+    }
+}
